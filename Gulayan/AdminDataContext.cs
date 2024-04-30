3bb5aa296d0a0b2d29cdc424a87841e86632cade
codingKlyde <>
2024-04-30 09:12:19 +0800

    Code refactor

--- Gulayan/AdminDataContext.cs
+++ Gulayan/AdminDataContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Gulayan.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gulayan
 {
---
+++ Gulayan/Models/Admin.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gulayan.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int AdminID { get; set; }
+        public string AdminUsername { get; set; }
+        public string AdminPassword { get; set; }
+    }
+}
