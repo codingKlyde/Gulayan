7834a04501c04833a81cd524095ce20c35f15549
codingKlyde <>
2024-04-30 09:13:06 +0800

    Visual and logic for WindowCatalog

--- Gulayan/CatalogWindow.xaml.cs
+++ Gulayan/CatalogWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using Gulayan.Models;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
 
 namespace Gulayan
 {
@@ -9,6 +13,34 @@ namespace Gulayan
         public CatalogWindow()
         {
             InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr nWnd, int WMsg, IntPtr wParam, IntPtr lParam); 
+
+        private void stckpnlControlBar_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+
+        private void bttnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void bttnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+                this.WindowState = WindowState.Maximized;
+            else
+                this.WindowState = WindowState.Normal;
+        }
+
+        private void bttnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
 
         public void Read()
@@ -16,16 +48,13 @@ namespace Gulayan
             using (ProductDataContext context = new ProductDataContext())
             {
                 DatabaseProducts = context.Products.ToList();
-                ItemList.ItemsSource = DatabaseProducts;
+                lstvwProduct.ItemsSource = DatabaseProducts;
             }
-
         }
 
         private void ReadButton_Click(object sender, RoutedEventArgs e)
         {
             Read();
         }
-
-    
     }
 }
