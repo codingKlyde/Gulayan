d122326e38196d4c74e332a1c520c59ffb05ab54
codingKlyde <>
2024-04-30 09:12:37 +0800

    Demo control

---
+++ Gulayan/Controls/CustomTitleBar.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Gulayan.Controls
+{
+    public class CustomTitleBar : Control
+    {
+        static CustomTitleBar()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomTitleBar), new FrameworkPropertyMetadata(typeof(CustomTitleBar)));
+        }
+    }
+}
