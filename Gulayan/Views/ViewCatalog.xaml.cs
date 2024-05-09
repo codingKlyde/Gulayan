2e6f21ef01a95ded0cc72eb258b88fa618442d36
codingKlyde <>
2024-05-09 13:03:19 +0800

    Added child views for menu

---
+++ Gulayan/Views/ViewCatalog.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Gulayan.Models;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Gulayan.Views
+{
+    public partial class ViewCatalog : UserControl
+    {
+        public List<Product> DatabaseProducts { get; private set; }
+
+        public ViewCatalog()
+        {
+            InitializeComponent();
+        }
+        
+
+
+        public void Read()
+        {
+            using (ProductDataContext context = new ProductDataContext())
+            {
+                DatabaseProducts = context.Products.ToList();
+                lstvwProduct.ItemsSource = DatabaseProducts;
+            }
+        }
+
+        private void ReadButton_Click(object sender, RoutedEventArgs e)
+        {
+            Read();
+        }
+    }
+}
