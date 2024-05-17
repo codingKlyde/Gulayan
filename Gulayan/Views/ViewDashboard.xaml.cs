78e474de80e58d830c0aaa03405416e8561f696f
codingKlyde <>
2024-05-17 16:34:12 +0800

    Visual and logic tweaks

--- Gulayan/Views/ViewDashboard.xaml.cs
+++ Gulayan/Views/ViewDashboard.xaml.cs
@@ -1,28 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Collections.ObjectModel;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Gulayan.Views
 {
-    /// <summary>
-    /// Interaction logic for ViewDashboard.xaml
-    /// </summary>
     public partial class ViewDashboard : UserControl
     {
+        public class Vege
+        {
+            public string ID { get; set; }
+            public string Name { get; set; }
+            public string Character { get; set; }
+            public string Barcode { get; set; }
+            public string Price { get; set; }
+            public string Stock { get; set; }
+
+            public Brush BGColor { get; set; }
+        }
+
         public ViewDashboard()
         {
             InitializeComponent();
+
+            var converter = new BrushConverter();
+            ObservableCollection<Vege> veges = new ObservableCollection<Vege>();
+
+            veges.Add(new Vege { ID = "1", Name = "Potato", Character="P", Barcode = "123456789", Price = "11", Stock = "2131", BGColor = (Brush)converter.ConvertFromString("#1098ad") });
+            veges.Add(new Vege { ID = "2", Name = "Eggplant", Character = "E", Barcode = "987654321", Price = "15", Stock = "3031", BGColor = (Brush)converter.ConvertFromString("#1e88e5") });
+            veges.Add(new Vege { ID = "3", Name = "Pumpkin", Character = "P", Barcode = "112233445", Price = "30", Stock = "2531", BGColor = (Brush)converter.ConvertFromString("#ff8f00") });
+            veges.Add(new Vege { ID = "4", Name = "Tomato", Character = "T", Barcode = "000001111", Price = "7", Stock = "1231", BGColor = (Brush)converter.ConvertFromString("#ff5252") });
+            veges.Add(new Vege { ID = "5", Name = "Onion", Character = "O", Barcode = "332265812", Price = "10", Stock = "3201", BGColor = (Brush)converter.ConvertFromString("#0ca678") });
+        
+            dtgrdVegetable.ItemsSource = veges;
         }
     }
 }
--- Gulayan/WindowLogin.xaml.cs
+++ Gulayan/WindowLogin.xaml.cs
@@ -9,24 +9,44 @@ namespace Gulayan
             InitializeComponent();
         }
 
+        // CONTROL BAR
         private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
                 DragMove();
         }
-
         private void bttnMinimize_Click(object sender, RoutedEventArgs e)
         {
             WindowState = WindowState.Minimized;
         }
-
         private void bttnClose_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
 
-
-
+        // LOGIN 
+        private void txtblckUsername_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtbxUsername.Focus();
+        }
+        private void txtbxUsername_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtbxUsername.Text) && txtbxUsername.Text.Length > 0)
+                txtblckUsername.Visibility = Visibility.Visible;
+            else
+                txtblckUsername.Visibility = Visibility.Collapsed;
+        }
+        private void txtblckPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            psswrdbxPassword.Focus();
+        }
+        private void psswrdbxPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(psswrdbxPassword.Password) && psswrdbxPassword.Password.Length > 0)
+                txtblckPassword.Visibility = Visibility.Visible;
+            else
+                txtblckPassword.Visibility = Visibility.Collapsed;
+        }
         private void bttnSubmit_Click(object sender, RoutedEventArgs e)
         {
             var inputUsername = txtbxUsername.Text;
@@ -45,7 +65,6 @@ namespace Gulayan
                     MessageBox.Show("User not found!");
             }
         }
-
         public void GainAccess()
         {
             WindowMenu main = new WindowMenu();
